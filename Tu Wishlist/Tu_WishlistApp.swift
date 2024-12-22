7ea6b71015e2d86d84f28881ccc00e1823281fd8
Ahmadelgndy <74977392+a7madelgndy@users.noreply.github.com>
2024-12-23 00:46:38 +0200

    Initial Commit

---
+++ Tu Wishlist/Tu_WishlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tu_WishlistApp.swift
+//  Tu Wishlist
+//
+//  Created by Ahmed El Gndy on 23/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct Tu_WishlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
